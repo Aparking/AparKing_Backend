5f2b11715c1897124e373a5a468a1d5bceb4087a
PedroRuizAguilar <cocode2002@gmail.com>
2024-02-27 17:11:19 +0100

    Modelo de Aparking User

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class AparkingUser(models.Model):
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+
+    dni = models.CharField(max_length=8, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    phone = models.CharField(max_length=20, blank=True, null=True)
