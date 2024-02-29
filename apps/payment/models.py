1ae38a83191573e957aaacc74945d6f4778b71cf
marvicmar <marvicmar@alum.us.es>
2024-02-29 20:59:31 +0100

    feat: Add entity for payment

--- apps/payment/models.py
+++ apps/payment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from apps.authentication.models import CustomUser
+from apps.payment.enums import MemberType
+
+class Credit(models.Model):
+    value = models.IntegerField(blank=False, null=False)
+    creation_date = models.DateField(auto_now_add=True, blank=False, null=False)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=False, null=False)
+
+class MemberShip(models.Model):
+    start_date=models.DateTimeField(blank=False, null=False)
+    end_date=models.DateTimeField(blank=False, null=False)
+    type = models.CharField(max_length=16, choices=MemberType.choices(), default=MemberType.FREE, blank=False, null=False)
