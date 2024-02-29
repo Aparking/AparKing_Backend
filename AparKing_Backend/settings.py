3b62f195f7c2ffb03d7dc9637d0a6f095463a22d
marvicmar <marvicmar@alum.us.es>
2024-02-29 21:40:26 +0100

    feat: Add app gis

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
     "apps.authentication",
     "apps.booking",
     "apps.garagement",
