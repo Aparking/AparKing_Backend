2ec90876910d934244f8aca74e7267f3fea81eec
marvicmar <marvicmar@alum.us.es>
2024-03-01 15:29:23 +0100

    fix: location bbdd

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.gis',
     "apps.authentication",
     "apps.booking",
     "apps.garagement",
