1d300ecd2a1a26678f2b1ebc96896140d378f721
Yuuta094 <YvanSan094@gmail.com>
2024-03-02 19:38:20 +0100

    feat: Implementación de la creación del anuncio de garajes #8

--- apps/garagement/serializers.py
+++ apps/garagement/serializers.py
@@ -10,15 +10,37 @@ class ImageSerializer(serializers.ModelSerializer):
     class Meta:
        model = Image
        fields ='__all__'
+       
+    def create(self, validated_data):
+        return super().create(validated_data)
 
 class AvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
        model = Availability
        fields = '__all__'
 
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def validate(self, data):
+        if data['end_date'] <= data['start_date']:
+            raise serializers.ValidationError("end_date must be later than start_date")
+        return data
+    
 class GarageSerializer(serializers.ModelSerializer):
     address = AddressSerializer()
 
     class Meta:
         model = Garage
         fields = '__all__'
+
+    def create(self, validated_data):
+        address_data = validated_data.pop('address')
+        address = Address.objects.create(**address_data)
+        garage = Garage.objects.create(address=address, **validated_data)
+        return garage
+    
+    def validate(self, data):
+        if data['end_date'] <= data['start_date']:
+            raise serializers.ValidationError("end_date must be later than start_date")
+        return data
