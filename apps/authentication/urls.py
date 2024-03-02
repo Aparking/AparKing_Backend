0d990fbc14d8bf87a30b1e04faab0ec58cd4cacd
josejavier <josalcsan1@alum.us.es>
2024-03-02 18:35:26 +0100

    Feat: listado con busqueda y borrado de todos los usuarios #3

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.shortcuts import render
+
 from . import views
+# Create your views here.
+
 
 urlpatterns = [
     path('login/', views.auth_login, name='login'),
     path('register/', views.register, name='register'),
     path('logout/', views.auth_logout, name='logout'),
+      path('api/users', views.users_list),
+    #path(r'^api/users/(?P<pk>[0-9]+)$', views.users_detail),
 ]
