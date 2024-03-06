1c41b1d919b4080b348694da7c3e87e0f3c9e7f3
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2024-03-06 21:54:03 +0100

    conf: docker compose and actions #91

--- migrationsInit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python manage.py migrate --noinput
-# python manage.py flush --noinput # This will delete all data in the database
-# python manage.py loaddata initial_data.json # This will load the initial data
-python importCSV.py
