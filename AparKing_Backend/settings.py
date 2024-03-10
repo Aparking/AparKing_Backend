f8f5f770b8d4885980c23043ebba33b7d08fa171
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2024-03-10 20:01:22 +0100

    conf: first deploy configuration #101

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-!eb&x8z=%ac03t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
---
+++ docker/backendInit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+while ! nc -z database 5432; do
+    echo 'Waiting for PostgreSQL database to become available...'
+    sleep 5;
+done;
+echo 'Migrations complete. Starting backend...';
+
+python manage.py runserver 0.0.0.0:3000
