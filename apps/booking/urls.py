5d4f93fe5f2a645fc67353c35538302eef0910c4
MarioArocaPaez <marioap2002@gmail.com>
2024-03-17 13:24:58 +0100

    Feat: Claim creation #32

--- apps/booking/urls.py
+++ apps/booking/urls.py
@@ -1,1 +1,6 @@
 from django.urls import path
+from .views import create_claim
+
+urlpatterns = [
+    path('claims/create/', create_claim, name='create_claim'),
+]
--- apps/booking/views.py
+++ apps/booking/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Claim
+from .serializers import ClaimSerializer
 
-# Create your views here.
+@api_view(['POST'])
+def create_claim(request) -> Response:
+    serializer = ClaimSerializer(data=request.data)
+    if serializer.is_valid():
+        claim = serializer.save()
+        return Response({'status': 'success', 'claim_id': claim.id})
+    else:
+        return Response(serializer.errors, status=400)
+
