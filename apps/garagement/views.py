814e4768adc4d8ac30f9253d3aa574b2599c71cd
laurolmer <laurolmer@alum.us.es>
2024-03-18 17:49:08 +0100

    refactor: Corrected destroy method

--- apps/garagement/views.py
+++ apps/garagement/views.py
@@ -25,7 +25,12 @@ class GarageViewSet(ViewSet):
         pass
 
     def destroy(self, request, pk=None):
-        pass
+        try:
+            garage = Garage.objects.get(pk=pk)
+            garage.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Garage.DoesNotExist:
+            return Response({"error": "Garaje no encontrado"}, status=status.HTTP_404_NOT_FOUND)
 
 
 # class GarageListCreateAPIView(ListCreateAPIView):
