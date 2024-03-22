6054af9592cb3447f92fc5751e5f963cccec18dc
MarioArocaPaez <marioap2002@gmail.com>
2024-03-22 15:53:15 +0100

    fix: codacy code failures

--- apps/garagement/urls.py
+++ apps/garagement/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     path('garages/', views.list_garage, name='garages'),
     path('garages/create/', views.create_garage, name='create_garage'),
-    
     path('garages/images/create/', views.create_image, name='create_image'),
     path('garages/images/', views.list_image, name='list_image'),
 ]
