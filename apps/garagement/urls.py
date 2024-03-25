1685f28f413a4b976e6cdcd4142fa6b96d3a0832
3ralon <albperleo@alum.us.es>
2024-03-25 13:06:10 +0100

    fix: urls for garagement and dependencies

--- apps/garagement/urls.py
+++ apps/garagement/urls.py
@@ -4,9 +4,10 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("garages/", views.list_garages, name="garages"),
-    path("garages/mine", views.list_my_garages, name="my_garages"),
-    path("garages/create/", views.create_garage, name="create_garage"),
-    path("garages/images/create/", views.create_image, name="create_image"),
-    path("garages/images/", views.list_image, name="list_image"),
+    path("", views.list_garages, name="garages"),
+    path("<int:pk>/", views.get_garage, name="garage"),
+    path("mine/", views.list_my_garages, name="my_garages"),
+    path("create/", views.create_garage, name="create_garage"),
+    path("images/create/", views.create_image, name="create_image"),
+    path("images/", views.list_image, name="list_image"),
 ]
