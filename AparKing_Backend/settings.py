6396a858e86711d3f4c79b72c1003ed6f70de8bd
sergiosant02 <sergiosantiago0403@gmail.com>
2024-03-27 19:24:39 +0100

    fix: default email and password #164

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -184,8 +184,8 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  config('EMAIL_HOST_USER', '')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', '')
+EMAIL_HOST_USER =  config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 
 
 
