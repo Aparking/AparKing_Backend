caa1f0a3f5e49b8eabec953d26433f0345a25a81
3ralon <albperleo@alum.us.es>
2024-03-29 20:12:57 +0100

    fix: update media root path and add static URL configuration #9

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
