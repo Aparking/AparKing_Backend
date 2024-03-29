a09a81ac86abc8bba557e224ac6e1dee8ea9c88a
3ralon <albperleo@alum.us.es>
2024-03-29 19:17:37 +0100

    fix: duplicate MEDIA_URL and MEDIA_ROOT definitions

--- AparKing_Backend/settings.py
+++ AparKing_Backend/settings.py
@@ -170,12 +170,6 @@ STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-# Base url to serve media files
-MEDIA_URL = "/media/"
-
-# Path where media is stored
-MEDIA_ROOT = BASE_DIR / "media/"
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
