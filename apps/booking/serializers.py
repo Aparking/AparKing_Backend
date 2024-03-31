6386293209ee8f44caf8e95bf0ef0b3c89b9f2e7
MarioArocaPaez <mararopae@alum.us.es>
2024-03-31 21:42:48 +0200

    fix: Garage rental reviews #12

--- apps/booking/serializers.py
+++ apps/booking/serializers.py
@@ -2,18 +2,19 @@ from apps.authentication.models import CustomUser
 from apps.booking.enums import BookingStatus, PaymentMethod
 from apps.booking.models import Book
 from apps.garagement.enums import GarageStatus
-from apps.garagement.serializers import AvailabilitySerializer, GarageSerializer
-from . import validations
 from apps.garagement.models import Availability, Garage
+from apps.garagement.serializers import AvailabilitySerializer, GarageSerializer
 from rest_framework import serializers
 
+from . import validations
+from .models import Comment
 
 
 class AvailabilitySerializer(serializers.ModelSerializer):
     class Meta:
-       model = Availability
-       fields = '__all__'
-       
+        model = Availability
+        fields = '__all__'
+
     def validate(self, attrs):
         return validations.validate_availability_data(attrs)
 
@@ -22,17 +23,15 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['payment_method', 'status', 'user', 'availability']
-        
+
     def validate(self, attrs):
         return validations.validate_booking_data(attrs)
-    
+
     def create(self, validated_data):
         availability = validated_data.pop('availability')
         booking = Book.objects.create(availability=availability, **validated_data)
         return booking
 
-from rest_framework import serializers
-from .models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
