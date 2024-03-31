76654a18c156ab1c70c0b7cf45e2ad41a4d09a81
laurolmer <laurolmer@alum.us.es>
2024-03-31 20:36:10 +0200

    fix: Listing and creation now working properly

--- apps/booking/views.py
+++ apps/booking/views.py
@@ -11,34 +11,29 @@ from apps.garagement.enums import GarageStatus
 from apps.booking.enums import BookingStatus
 
         
-@api_view(['POST'])
+@api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def create_booking(request):
-    if request.method == 'POST':
-        data = request.data
-        user = request.user
-        availability = get_object_or_404(Availability, id=data['availability'])
-        garage = get_object_or_404(Garage, id=availability.garage.id)
-        availability.status = GarageStatus.RESERVED.value
-        availability.save()
+    if request.method == "POST":
+        book_serializer = BookSerializer(data=request.data)
+        if book_serializer.is_valid():
+            book_serializer.save()
+            return Response(book_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(book_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        book = Book.objects.create(payment_method=data['payment_method'], status=BookingStatus.CONFIRMED.value, user=user, availability=availability)
-        bookSerializer = BookSerializer(book)
-        bookSerializer.save()
-        return Response(bookSerializer.data, status=status.HTTP_201_CREATED)
-
-@api_view(['GET'])
+@api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def list_my_bookings(request):
-    if request.method == 'GET':
+    if request.method == "GET":
         user = request.user
         bookings = Book.objects.filter(user=user)
         if bookings.exists():
             serializer = BookSerializer(bookings, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response({'message': 'No se encontraron reservas de garajes.'}, status=status.HTTP_404_NOT_FOUND)
-         
+            return Response({"message": "No se encontraron reservas de garajes."}, status=status.HTTP_404_NOT_FOUND,)
+            
 @api_view(["GET", "DELETE"])
 @permission_classes([IsAuthenticated])
 def booking_details(request, pk):
