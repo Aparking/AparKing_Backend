52452823b4518011a593298d0d407a80d7df879b
3ralon <albperleo@alum.us.es>
2024-04-08 17:51:36 +0200

    feat: get user authenticated info #208

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,24 +1,25 @@
 from rest_framework.decorators import api_view
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
-from django.contrib.auth import login, logout, authenticate
-from .serializers import LoginSerializer, RegisterSerializer
+from .serializers import CustomUserSerializer, LoginSerializer, RegisterSerializer
 from apps.mailer import generic_sender as Mailer
 from apps.utils import code_generator
+from apps.authentication.models import CustomUser
 
-@api_view(['POST'])
+
+@api_view(["POST"])
 def auth_login(request) -> Response:
     serializer = LoginSerializer(data=request.data)
     if serializer.is_valid():
         user = serializer.validated_data
         Token.objects.filter(user=user).delete()
         token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=200)
+        return Response({"token": token.key}, status=200)
     else:
         return Response(serializer.errors, status=400)
-    
-@api_view(['POST'])
+
+
+@api_view(["POST"])
 def delete_account(request) -> Response:
     try:
         token = Token.objects.get(key=request.data["token"])
@@ -30,19 +31,20 @@ def delete_account(request) -> Response:
             return Response(status=400)
     except Exception:
         return Response(status=400)
-    
-@api_view(['POST'])
+
+
+@api_view(["POST"])
 def verify_user(request) -> Response:
     try:
         token = Token.objects.get(key=request.data["token"])
         if token:
             user = token.user
-            if user.code == request.data['code']:
+            if user.code == request.data["code"]:
                 user.is_active = True
                 user.save()
                 token.delete()
                 token, _ = Token.objects.get_or_create(user=user)
-                return Response({'token':token.key}, status=200)
+                return Response({"token": token.key}, status=200)
             else:
                 return Response(status=400)
         else:
@@ -51,8 +53,17 @@ def verify_user(request) -> Response:
         return Response({"error": "Token not found"}, status=400)
 
 
-    
-@api_view(['POST'])
+@api_view(["GET"])
+def user_info(request) -> Response:
+    user = request.user
+    if user.is_authenticated:
+        custom_user = CustomUser.objects.get(id=user.id)
+        serialized = CustomUserSerializer(custom_user)
+        return Response(serialized.data, status=200)
+    return Response(status=401)
+
+
+@api_view(["POST"])
 def register(request) -> Response:
     serializer = RegisterSerializer(data=request.data)
     if serializer.is_valid():
@@ -61,16 +72,17 @@ def register(request) -> Response:
         user.code = code_generator.code_generator(10)
         token, _ = Token.objects.get_or_create(user=user)
         Mailer.send_email(
-            subject=f'AparKing - Activar cuenta',
-            message=f'Bienvenido {user.first_name}, para activar su cuenta introduzca el siguiente código: {user.code}',
-            mail_to=user.email
+            subject=f"AparKing - Activar cuenta",
+            message=f"Bienvenido {user.first_name}, para activar su cuenta introduzca el siguiente código: {user.code}",
+            mail_to=user.email,
         )
         user.save()
-        return Response({'token':token.key}, status=200)
+        return Response({"token": token.key}, status=200)
     else:
         return Response(serializer.errors, status=400)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def auth_logout(request) -> Response:
     user = request.user
     if user.is_authenticated:
