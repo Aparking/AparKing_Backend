789eff64814b07da75b815042d0cfbcec7699cb8
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2024-04-22 21:28:37 +0200

    deploy: fix populate

--- docker/backendInit.sh
+++ docker/backendInit.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+# Espera hasta que la base de datos esté disponible
 while ! nc -z database 5432; do
     echo 'Waiting for PostgreSQL database to become available...'
-    sleep 5;
-done;
-echo 'Migrations complete. Starting backend...';
+    sleep 5
+done
+echo 'Database is available.'
 
+# Inicia el servidor Django en el fondo temporalmente
+echo 'Starting backend...'
+python manage.py runserver 0.0.0.0:3000 &
+
+# Almacena el ID del proceso
+SERVER_PID=$!
+
+sleep 10
+
+# Carga los datos
+echo 'Loading data...'
 python manage.py loaddata Populate.json
-python manage.py runserver 0.0.0.0:3000
+
+# Trae el servidor de nuevo al primer plano
+wait $SERVER_PID
