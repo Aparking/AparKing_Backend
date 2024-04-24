4e6dc77f4b8773702124363428952e2440ff65b4
Juan Carlos Ramírez López <juancarlosralop@gmail.com>
2024-04-24 19:47:26 +0200

    deploy: config

--- AparKing_Backend/docker_settings.py
+++ AparKing_Backend/docker_settings.py
@@ -12,14 +12,5 @@ DATABASES = {
 	}
 }
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("redis", 6379)],
-        },
-    },
-}
-
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH', '/usr/lib/x86_64-linux-gnu/libgdal.so')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH', '/usr/lib/x86_64-linux-gnu/libgeos_c.so')
